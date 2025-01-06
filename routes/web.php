5189220575891fe7ec7b6beb0222e8138f88812d
Adam H.M <mr.r4t25@gmail.com>
2025-01-06 20:59:36 +0700

    feat(routes): defining route resource for Mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\ProgramStudiController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -13,3 +14,4 @@ Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::resource('program-studi', ProgramStudiController::class);
+Route::resource('mahasiswa', MahasiswaController::class);
