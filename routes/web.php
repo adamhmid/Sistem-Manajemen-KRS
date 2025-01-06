fc18969858a9710f477cdd4c094131afd799bc13
Adam H.M <mr.r4t25@gmail.com>
2025-01-06 21:59:09 +0700

    feat(routes): defining route resource for Mata Kuliah

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MataKuliahController;
 use App\Http\Controllers\ProgramStudiController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -17,3 +18,4 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::resource('program-studi', ProgramStudiController::class);
 Route::resource('mahasiswa', MahasiswaController::class);
 Route::resource('dosen', DosenController::class);
+Route::resource('mata-kuliah', MataKuliahController::class);
