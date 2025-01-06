713ba98e6c870eb313d07f7cc3af41a8460d6e4a
Adam H.M <mr.r4t25@gmail.com>
2025-01-06 21:34:04 +0700

    feat(routes): defining route resource for Dosen

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DosenController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\ProgramStudiController;
@@ -15,3 +16,4 @@ Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::resource('program-studi', ProgramStudiController::class);
 Route::resource('mahasiswa', MahasiswaController::class);
+Route::resource('dosen', DosenController::class);
