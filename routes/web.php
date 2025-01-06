5c90a55094f8bce4e83b85a7b6345ae1ff4e7021
Adam H.M <mr.r4t25@gmail.com>
2025-01-06 22:56:23 +0700

    feat(routes): defining route resource for Kelas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KelasController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MataKuliahController;
 use App\Http\Controllers\ProgramStudiController;
@@ -19,3 +20,4 @@ Route::resource('program-studi', ProgramStudiController::class);
 Route::resource('mahasiswa', MahasiswaController::class);
 Route::resource('dosen', DosenController::class);
 Route::resource('mata-kuliah', MataKuliahController::class);
+Route::resource('kelas', KelasController::class);
