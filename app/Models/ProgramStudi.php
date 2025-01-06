0f05a66ee3da3e4e3407b2d1f87251233e83866e
Adam H.M <mr.r4t25@gmail.com>
2025-01-06 19:26:22 +0700

    refactor(models): add guarded attribute for aren't mass assignable

--- app/Models/ProgramStudi.php
+++ app/Models/ProgramStudi.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ProgramStudi extends Model
 {
+  protected $guarded = [];
+
   public function mahasiswa(): HasMany
   {
     return $this->hasMany(Mahasiswa::class);
