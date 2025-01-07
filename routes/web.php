2a8fab494a6acaf3c1b93c29a5ca443c4aa1d1cb
Adam H.M <mr.r4t25@gmail.com>
2025-01-07 16:03:24 +0700

    refactor(routes): defining routes middleware for auth & role

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,24 @@ use App\Http\Controllers\RegistrasiKuliahController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+  return view('welcome');
+});
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::resource('program-studi', ProgramStudiController::class);
-Route::resource('mahasiswa', MahasiswaController::class);
-Route::resource('dosen', DosenController::class);
-Route::resource('mata-kuliah', MataKuliahController::class);
-Route::resource('kelas', KelasController::class);
-Route::resource('registrasi-kuliah', RegistrasiKuliahController::class);
+Route::middleware('auth')->group(function () {
+  Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+  Route::middleware(['role:admin'])->group(function () {
+    Route::resource('program-studi', ProgramStudiController::class);
+    Route::resource('mahasiswa', MahasiswaController::class);
+    Route::resource('dosen', DosenController::class);
+    Route::resource('mata-kuliah', MataKuliahController::class);
+    Route::resource('kelas', KelasController::class);
+  });
+
+  Route::middleware(['role:admin,operator'])->group(function () {
+    Route::resource('registrasi-kuliah', RegistrasiKuliahController::class);
+  });
+});
